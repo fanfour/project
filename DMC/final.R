3e88b19e2cd724c967c53b3924f9deb4bd9ef852
MANUEL-F-LAPTOP\Manuel <manuel.freytag26@gmail.com>
2017-05-06 16:45:51 +0200

    Adding the regression model and trying to combine the results
    However, there is a flaw in the result combination!

--- DMC/final.R
+++ DMC/final.R
@@ -6,8 +6,6 @@ library(sqldf)
 set.seed(42)
 options(scipen = 999)
 
-
-
 #Installations necessary?
 if(FALSE){
   install.packages("penalized")
@@ -16,26 +14,19 @@ if(FALSE){
   install.packages("adabag")
 }
 
-sample = read.csv2("01_Data/final_sample.csv")
 
 #DATA IMPORT AND PREPARATION
-originalDataImport = function(folder){
+originalDataImport = function(){
   
-  train_org = read.csv(paste(folder, "train.csv", sep = ""), sep = "|", header = TRUE, dec = ".")
-  items = read.csv(paste(folder, "items.csv", sep = ""), sep = "|", header = TRUE, dec = ".")
-  
-  train_org[is.na(train_org)] = 0
-  items[is.na(items)] = "MISSING"
-  levels(items$campaignIndex)[1] = "MISSING"
-  
-  train_org = subset(sqldf("select * from train_org join items on train_org.pid = items.pid"), select = -c(3))
+  train_org = read.csv2("joined_train_org_sample.csv")
+  names(train_org)[names(train_org)=="revenue"] = "revenue_Clean"
   
   #Remove false predictors
-  train_org = subset(train_org, select = -c(pid, lineID, revenue, click, basket))
+  train_org = subset(train_org, select = -c(X, pid, lineID))
   
   #factor data
   train_org.factor = c("adFlag", "availability", "manufacturer", "group", "content", "unit", 
-                       "genericProduct", "salesIndex", "category", "campaignIndex", "order")
+                       "genericProduct", "salesIndex", "category", "campaignIndex")
   
   for(i in train_org.factor){
     train_org[i] = as.factor(train_org[[i]])
@@ -43,19 +34,19 @@ originalDataImport = function(folder){
   
   return(train_org)
 }
-prepDataImport = function(folder){
-  train = read.csv2(paste(folder, "final_sample.csv", sep = ""))
+prepDataImport = function(){
+  train = read.csv("mergedTrain_sample.csv")
   
   names(train)[names(train)=="order_Clean"] = "order"
   
   #Remove false predictors
-  train = subset(train, select = -c(X, lineID, day_Clean, revenue_Clean, content_Clean))
+  train = subset(train, select = -c(X, pid_Clean, lineID))
   
   #WEEKDAYS ARE MISSING
-  train.factor_fix = c("adFlag_Clean", "availability_Clean", "order", "pharmForm_Clean", 
-                       "genericProduct_Clean", "salesIndex_Clean", "missingCompetitorPrice")
+  train.factor_fix = c("adFlag_Clean", "availability_Clean", "pharmForm_Clean", 
+                       "genericProduct_Clean", "salesIndex_Clean", "missingCompetitorPrice", "weekdays")
   
-  train.factor_rem = c("manufacturer_Clean", "group_Clean", "unit_Clean", "category_Clean", "campaignIndex_Clean")
+  train.factor_rem = c("manufacturer_Clean", "group_Clean", "content_Clean", "unit_Clean", "category_Clean", "campaignIndex_Clean")
   
   train.factor = c(train.factor_fix, train.factor_rem)
   
@@ -65,7 +56,7 @@ prepDataImport = function(folder){
   
   return(train)
 }
-sets = list(originalDataImport("01_Data/"), prepDataImport("01_Data/"))
+sets = list(originalDataImport(), prepDataImport())
 
 removeIDLikeFactors = function(listSets, maxNrFactors){
   #Remove all factorial attributes with nr(factors) > 50
@@ -81,52 +72,42 @@ removeIDLikeFactors = function(listSets, maxNrFactors){
     listSets[[i]] = listSets[[i]][, (! colnames(listSets[[i]]) %in% rem)]
   }
   
+  
   return(listSets)
 }
 sets = removeIDLikeFactors(sets, 50)
 
-#Sample selection
-#Get uniform sample
-uniformSampling = function(listSets, size){
-  row_sample = sample(rownames(listSets[[1]]), size)
-  
-  for(i in c(1:length(listSets))){
-    row_sample = sample(rownames(listSets[[i]]), size)
-    
-    listSets[[i]] = listSets[[i]][row_sample,]
-  }
-  return(listSets)
-}
-sets = uniformSampling(sets, 20000)
-
 #MODEL PREPARATION
 rdesc = makeResampleDesc("CV", iters = 3)
 #learner selection
-learners = list(makeLearner("classif.OneR"),
-                makeFeatSelWrapper("classif.C50", resampling = rdesc, control = makeFeatSelControlGA(maxit = 10, mutation.rate = 0.1)))
+learners = list(makeLearner("classif.OneR"))
+
+#makeFeatSelWrapper("classif.C50", resampling = rdesc, control = makeFeatSelControlGA(maxit = 10, mutation.rate = 0.1))
 
 #makeLearner("classif.boosting")
 #makeFeatSelWrapper("classif.randomForest", resampling = rdesc, control = makeFeatSelControlGA(maxit = 10, mutation.rate = 0.1))
 
 #parameter_sets if necessary
 #getParamSet("classif.penalized.lasso")
-lasso.learner = makeLearner("classif.penalized.lasso")
-lasso.learner = setHyperPars(lasso.learner, lambda1 = 10, trace = TRUE)
-lasso.wrapper = makeFeatSelWrapper(lasso.learner, resampling = rdesc, control = makeFeatSelControlGA(maxit = 10, mutation.rate = 0.1))
+#lasso.learner = makeLearner("classif.penalized.lasso")
+#lasso.learner = setHyperPars(lasso.learner, lambda1 = 10, trace = TRUE)
+#lasso.wrapper = makeFeatSelWrapper(lasso.learner, resampling = rdesc, control = makeFeatSelControlGA(maxit = 10, mutation.rate = 0.1))
 
-learners = append(learners, list(lasso.wrapper))
+#learners = append(learners, list(lasso.wrapper))
 
 #make tasks
-makeTasks = function(listSets, target){
+makeClassifTasks = function(listSets, target, rem = NULL){
   tasks = list()
   
   for(i in c(1:length(listSets))){
-    tasks = append(tasks, list(makeClassifTask(id = as.character(i), data = listSets[[i]], target = target)))
+    tasks = append(tasks, list(makeClassifTask(id = as.character(i), data = subset(listSets[[i]], select = -c(quantity, revenue_Clean)), target = target)))
   }
   
   return(tasks)
 }
-tasks = makeTasks(sets, "order")
+
+#Remove quantity for prediction
+tasks = makeClassifTasks(sets, "order")
 
 #RESAMPLING / TESTING DECSISION
 rdesc2 = makeResampleDesc("CV", iters = 5)
@@ -137,10 +118,111 @@ rdesc2 = makeResampleDesc("CV", iters = 5)
 parallelStartSocket(2, level = "mlr.resample")
 #Setting up a benchmarking experiment
 
-bmr = benchmark(learners, tasks, rdesc2, list(acc, fpr, fnr))
+bmr = benchmark(learners, tasks, rdesc2, list(acc))
 
 parallelStop()
-write.csv(bmr, "test.csv")
+
+write.csv(bmr, "classification_results.csv")
+
+
+
+#REGRESION OVER QUANTITY
+reg_learners = list(makeLearner("regr.lm"))
+
+makeRegrTasks = function(listSets, target){
+  regTasks = list()
+  
+  for(i in c(1:length(listSets))){
+    newTask = makeRegrTask(id = as.character(i), data = subset(listSets[[i]], select = -c(order, revenue_Clean)), target = target)
+    regTasks = append(regTasks, list(newTask))
+  }
+  
+  return(regTasks)
+}
+
+#make reduced sets for the model training
+redSets = list()
+for(i in c(1:length(sets))){
+  #1) get all with quantity > 0
+  
+  tmp = createDummyFeatures(sets[[i]])
+  
+  tmp = tmp[which(tmp$quantity > 0),]
+  
+  redSets = append(redSets, list(tmp))
+  
+}
+
+regTasks.learn = makeRegrTasks(redSets, "quantity")
+
+parallelStartSocket(2, level = "mlr.resample")
+
+bmr_reg = benchmark(learners = reg_learners, tasks = regTasks.learn)
+
+parallelStop()
+
+
+
+#Final evaluation
+
+rmse_own = function(actual, predicted){
+  error <- actual - predicted
+  rmse = sqrt(mean(error^2))
+  
+  return(rmse)
+}
+
+
+#predict(bmr_reg$results$`1`$regr.lm$models[[1]], regTasks[[1]])
+
+revenue_predictions = function(sets, bmr_class, bmr_rev){
+  #create predict tasks
+  tmp_sets = list()
+  for(i in c(1:length(sets))){
+    tmp = createDummyFeatures(sets[[i]])
+    tmp_sets = append(tmp_sets, list(tmp))
+    
+  }
+  regTasks.pred = makeRegrTasks(tmp_sets, "quantity")
+  
+  rev_predictions = list()
+  
+  for(i in bmr_class$results){
+    
+    #Iterating over the tasks
+    for(j in length(i)){
+      tmp = subset(sets[[j]], select = c(revenue_Clean))
+      
+
+      
+      #reduce it to the size of regr
+      
+      
+      for(k in bmr_reg$results){
+          tmp["ID"] = i[[j]]$pred$data$id
+          tmp["numericPred"] = predict(k[[j]]$models[[1]], regTasks.pred[[j]])
+          tmp["classPred"] = as.numeric(i[[j]]$pred$data$response)
+          
+          rev_predictions = append(rev_predictions, list(tmp))
+      }
+    }
+  }
+  
+  return(rev_predictions)
+}
+
+
+rev_preds = revenue_predictions(sets, bmr, bmr_reg)
+
+
+rev_preds[1]
+
+
+
+final_ev = data.frame(classifier = character(), regression = character(), rmse = integer())
+
+bmr_reg$results$`1`$regr.lm$models[[1]]
+
 #Final evaluation of the models
 #MSE
 
